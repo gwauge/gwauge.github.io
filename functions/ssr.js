9c098f8eb066c4b82d51d372941d1eca395b9917
Georg Auge <georg.auge01@gmail.com>
2020-11-06 18:04:01 +0100

    added html string to ssr function

--- functions/ssr.js
+++ functions/ssr.js
@@ -2,8 +2,20 @@ exports.handler = async function (event, context) {
     return {
         statusCode: 200,
         headers: {
-            "Content-Type": 'text/plain; charset=UTF-8',
+            "Content-Type": 'text/html; charset=UTF-8',
         },
-        body: 'test string'
+        body: `
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>My SSR site</title>
+                </head>
+                <body>
+                    <h1>Hello, and welcome!</h1>
+                </body>
+            </html>
+        `
     };
 }
